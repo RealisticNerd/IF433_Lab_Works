1ae85f804f862a1540b1b84b0e3a5288455749e2
RealisticNerd <muhammadrizkyhardani05@gmail.com>
2026-09-04 21:13:48 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_160870_MuhammadRizkyHardani/week01/GameStore.kt
+++ src/oop_160870_MuhammadRizkyHardani/week01/GameStore.kt
@@ -1,2 +1,10 @@
 package oop_160870_MuhammadRizkyHardani.week01
 
+fun main(){
+    val gameTitle: String = "Cyberpunk 2077"
+    val price: Int = 600000
+
+}
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) price-price*20/100 else price-price*10/100
+
