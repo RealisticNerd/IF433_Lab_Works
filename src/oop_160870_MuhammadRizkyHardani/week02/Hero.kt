489c7785e3b3c9e00918b9a9ab3a1ea187d49444
RealisticNerd <muhammadrizkyhardani05@gmail.com>
2026-09-11 17:49:05 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_160870_MuhammadRizkyHardani/week02/Hero.kt
@@ -0,0 +1,14 @@
+package oop_160870_MuhammadRizkyHardani.week02
+
+class Hero(
+    val name: String,
+    var hp: Int,
+    var baseDamage: Int = 100
+){
+    fun attack(targetName: String){
+        println("")
+    }
+    fun takeDamage(damage: Int){
+
+    }
+}
