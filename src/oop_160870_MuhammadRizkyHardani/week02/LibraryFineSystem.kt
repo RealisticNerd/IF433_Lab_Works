53868bad7a3a1e2eae689a1c76d2b0f6af2f2150
RealisticNerd <muhammadrizkyhardani05@gmail.com>
2026-09-11 12:23:44 +0700

    week02: (task 1) implement library input system

--- src/oop_160870_MuhammadRizkyHardani/week02/LibraryFineSystem.kt
+++ src/oop_160870_MuhammadRizkyHardani/week02/LibraryFineSystem.kt
@@ -1,2 +1,41 @@
 package oop_160870_MuhammadRizkyHardani.week02
 
+import java.util.Scanner
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    var loanDurration: Int = 1
+) {
+    fun calculateFine(): Int {
+        return if (loanDurration > 3) {
+            (loanDurration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    print("Judul Buku: ")
+    val judul = scanner.nextLine()
+
+    print("Nama Peminjam: ")
+    val name = scanner.nextLine()
+
+    print("Lama Pinjam (Hari): ")
+    var lama = scanner.nextInt()
+    if (lama < 0) {
+        lama = 1
+    }
+    val loan = Loan(judul, name, lama)
+    val denda = loan.calculateFine()
+
+    println("Detail Peminjaman")
+    println("Judul Buku    : ${loan.bookTitle}")
+    println("Nama Peminjam : ${loan.borrower}")
+    println("Lama Pinjam   : ${loan.loanDurration} hari")
+    println("Total Denda   : Rp. $denda")
+}
