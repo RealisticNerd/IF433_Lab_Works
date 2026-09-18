d9e71fffec45025a137db1cbf6413f956952c247
RealisticNerd <muhammadrizkyhardani05@gmail.com>
2026-09-18 12:31:02 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_160870_MuhammadRizkyHardani/week03/tugas2/Main.kt
+++ src/oop_160870_MuhammadRizkyHardani/week03/tugas2/Main.kt
@@ -1,2 +1,7 @@
 package oop_160870_MuhammadRizkyHardani.week03.tugas2
 
+fun main(){
+    val player = Player("Rizz", 12)
+
+    player.addXp(100)
+}
--- src/oop_160870_MuhammadRizkyHardani/week03/tugas2/Player.kt
+++ src/oop_160870_MuhammadRizkyHardani/week03/tugas2/Player.kt
@@ -1,2 +1,19 @@
 package oop_160870_MuhammadRizkyHardani.week03.tugas2
 
+class Player (val username: String, private var xp: Int = 0) {
+    val level: Int
+    get() = (xp/100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount <= 0) {
+            println("Hanya Menerima angka Positif")
+            return
+        }
+
+        val oldLevel = level
+        xp += amount
+        if (level > oldLevel){
+            println("Level Up! Selamat $username naik ke level $level")
+        }
+    }
+}
