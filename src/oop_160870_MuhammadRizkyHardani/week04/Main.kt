fafa4447030814119d998459f46659990e683320
RealisticNerd <muhammadrizkyhardani05@gmail.com>
2026-09-19 10:13:29 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_160870_MuhammadRizkyHardani/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_160870_MuhammadRizkyHardani.week04
+
+fun main(){
+    println("--- Testing Vehicle ---")
+    val generateVehicle = Vehicle("Sepeda Onthel")
+    generateVehicle.honk()
+    generateVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
