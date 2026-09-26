7b27e7e9ff578025bb938bc668c6d30c45b12ab0
RealisticNerd <muhammadrizkyhardani05@gmail.com>
2026-09-26 11:17:35 +0700

    week05: (task 2) create EWallet class

--- src/oop_160870_MuhammadRizkyHardani/week05/tugas2/EWallet.kt
+++ src/oop_160870_MuhammadRizkyHardani/week05/tugas2/EWallet.kt
@@ -1,2 +1,21 @@
 package oop_160870_MuhammadRizkyHardani.week05.tugas2
 
+class EWallet (accountName: String, var balance: Double): PaymentMethod(accountName){
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            println("Sukses")
+            balance -= amount
+        } else {
+            println("Saldo Tidak Cukup")
+        }
+
+        fun topUp(amount: Double) {
+            if (amount > 0) {
+                balance += amount
+                println("Top up berhasil!")
+            } else {
+                println("Jumlah top up tidak valid.")
+            }
+        }
+    }
+}
