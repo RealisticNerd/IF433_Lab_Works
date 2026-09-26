095d97b101433f9448da256fe2c38c6dfc980e9c
RealisticNerd <muhammadrizkyhardani05@gmail.com>
2026-09-26 10:48:42 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_160870_MuhammadRizkyHardani/week05/tugas1/Main.kt
@@ -0,0 +1,11 @@
+package oop_160870_MuhammadRizkyHardani.week05.tugas1
+
+fun main(){
+    val math = MathHelper()
+
+    println("Jika sisi kotak sepanjang 2m maka luasnya: ${math.hitungLuas(2)}m")
+
+    println("Jika balok dengan panjang 3m dan lebar 2m maka luasnya: ${math.hitungLuas(3, 2)}m")
+
+    println("Jika sebuah lingkaran memiliki panjang jari jari 7m luasnya: ${math.hitungLuas(7.0)}m")
+}
